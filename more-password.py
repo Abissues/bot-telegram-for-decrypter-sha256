a145c2bda0fdeefd176a76e4cba0d9d09dde7f8a
abissues <53044683+Abissues@users.noreply.github.com>
2020-01-12 18:21:51 +0100

    Update more-password.py

--- more-password.py
+++ more-password.py
@@ -1,36 +1,40 @@
 import random
 import os
-catto=0
 while 1:
-    catto=catto+1
-    print(catto)
-    passlen = 1
-    data="QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm@#1234567890"
-    p =  "".join(random.sample(data,passlen ))
-    leggi=open("password.txt",'r')
-    l=leggi.readlines()
-    if p in l:
-        a=open("aumento.txt","r")
-        n=a.readline()
-        n=int(n)
-        n=n+1
-        a.close()
-        a=open("aumento.txt","w")
-        a.write(str(n))
-        a.close()       
-        a=open("aumento.txt","r")
-        n=a.readline()
-        n=int(n)
-        if n>100:
-            n=n*0
-            passlen+=1
-            a.close()
-            a=open("aumento.txt","w")
-            a.write(str(n))
-            a.close()
-        leggi.close()
-    else:
-        x=open("password.txt",'a')
-        xa=x.write(p+'\n')
-        x.close()
-        
+    s = "01234567890ABCDEFGHIJKLMNOPQRSTUVWXYZqwertyuiopasdfghjklzxcvbnm@#"
+    ary=open("arrivo.txt","r")
+    l=ary.readline()
+    l=int(l)
+    ary.close()
+    passlen = l
+    p =  "".join(random.sample(s,passlen ))
+    with open('password.txt') as file:
+        contents = file.read()
+        search_word = p
+        if search_word in contents:
+            print ('parola trovata non aggiungo nulla')
+            p=open("aumento.txt","r")
+            s=p.readline()
+            s=int(s)
+            p.close()
+            if s==100:
+                p=open("aumento.txt","w")
+                p.write('0')
+                p.close()
+                parte=open("arrivo.txt","r")
+                s=parte.readline()
+                s=int(s)
+                parte.close()
+                h=open("arrivo.txt","w")
+                s+=1
+                f=h.write(str(s))
+                h.close()
+            p=open("aumento.txt","w")
+            s=s+1
+            p.write(str(s))
+            p.close()
+
+        else:
+            testate = open('password.txt', 'a')
+            testate.write(p + '\n')
+            print("parola aggiunta")
